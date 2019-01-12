1759615265c36becb78e2f0c815d1d991a11f056
Oscar Rieken <oscar.rieken@adp.com>
2019-01-12 18:17:48 -0500

    feat(Soundex): now combines duplicate encodings

--- src/Soundex.h
+++ src/Soundex.h
@@ -11,28 +11,6 @@ public:
         return zeroPad(head(word) + encodedDigits(tail(word)));
     }
 
-private:
-    std::string tail(const std::string &word) const {
-        return word.substr(1);
-    }
-
-    std::string head(const std::string &word) const {
-        return word.substr(0, 1);
-    }
-
-    std::string encodedDigits(const std::string &word) const {
-        std::string encoding;
-        for (auto letter: word) {
-            if (isComplete(encoding)) break;
-            encoding += encodedDigit(letter);
-        }
-        return encoding;
-    }
-
-    bool isComplete(const std::string &encoding) const {
-        return encoding.length() == MaxCodeLength - 1;
-    }
-
     std::string encodedDigit(char letter) const {
         const std::unordered_map<char, std::string> encodings{
                 {'b', "1"},
@@ -58,6 +36,34 @@ private:
         auto it = encodings.find(letter);
 
         return it == encodings.end() ? "" : it->second;
+    }
+
+private:
+    std::string tail(const std::string &word) const {
+        return word.substr(1);
+    }
+
+    std::string head(const std::string &word) const {
+        return word.substr(0, 1);
+    }
+
+    std::string lastDigit(const std::string& encoding) const {
+        if(encoding.empty()) return "";
+        return std::string(1, encoding.back());
+    }
+
+    std::string encodedDigits(const std::string &word) const {
+        std::string encoding;
+        for (auto letter: word) {
+            if (isComplete(encoding)) break;
+            if (encodedDigit(letter) != lastDigit(encoding))
+            encoding += encodedDigit(letter);
+        }
+        return encoding;
+    }
+
+    bool isComplete(const std::string &encoding) const {
+        return encoding.length() == MaxCodeLength - 1;
     }
 
     static const size_t MaxCodeLength{4};
