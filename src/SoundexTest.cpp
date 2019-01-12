17f834f33663179f0e3706d1674b9e19a12ebeef
Oscar Rieken <oscar.rieken@adp.com>
2019-01-11 21:07:02 -0500

    refactor(test): added another test, renamed file for misspelling, 😬🐙

---
+++ src/SoundexTest.cpp
@@ -0,0 +1,20 @@
+#include "gmock/gmock.h"
+#include "Soundex.h"
+
+using ::testing::Eq;
+
+class SoundexEncoding: public testing::Test {
+public: Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+    ASSERT_THAT(soundex.encode("A"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+  ASSERT_THAT(soundex.encode("I"), Eq("I000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesConstantsWithAppropriateDigits) {
+    ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
+}
